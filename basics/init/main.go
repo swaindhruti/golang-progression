0fe33098c4a18f200a8a5f75b5e7ea76aef550b6
Dhrutinandan Swain <dhrut.24.swain@gmail.com>
2025-05-18 13:40:17 +0530

    Refactor init function examples with detailed comments and structured initialization phases

--- basics/init/main.go
+++ basics/init/main.go
@@ -1,15 +1,30 @@
 package main
 
 /*
-- In Golang, init functions are special functions that can be declared in any package.
-- They are used to perform initialization tasks before the package is used.
-- Go executes init functions automatically when the package is initialized.
-- This happens before the main function is executed.
+- In Golang, init functions are special functions that can be used for initialization tasks
+- They execute automatically when the package is initialized, before main
+- They run in the order they are defined in the source code
+- They take no parameters and return no values
 */
 
+// init functions can be grouped by purpose
 func init() {
-	// Initialization code can go here
-	println("Initializing...")
+	println("Initializing...1 - First phase")
+}
+
+// Separate init functions with clear comments about their purpose
+func init() {
+	println("Initializing...2 - Configuration")
+}
+
+// Each init function can handle a different aspect of initialization
+func init() {
+	println("Initializing...3 - Resource setup")
+}
+
+// Final initialization steps
+func init() {
+	println("Initializing...4 - Ready to start")
 }
 
 func main() {
